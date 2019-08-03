4a924c0837a2fc67277f03202e7f61793702f899
Sebastian Schmid <schmid.sebastian@mail.de>
2019-08-03 16:25:05 +0200

    Added alternative login methods as well as some docstring

--- coupon_activator.py
+++ coupon_activator.py
@@ -4,7 +4,23 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
 class Activator:
+    """Payback eCoupons are a nice way to collect more payback points (=money!). 
+    However, for them to be active, you need to login regularly and activate them 
+    one by one. By doing so, you come in contact with the marketing campaign 
+    connected to the eCoupon, which in turn might affect your shopping behaviour.
+    This library aims to automate the eCoupon activation process so you can 
+    collect more payback points without the drawback of being manipulated. 
     
+    Attributes:
+        - browser: "Edge", "Chrome", or "Firefox". This is going the be the browser
+                   that will be used when automating. More browsers will be added.
+        - login_style: "password", "pin" or "plz". Your way of logging into your
+                   payback account. 
+        - username: provide your username like "kundennummer" or "mail@mail.de".
+        - password: provide your password like "12345". ATTENTION: When using
+                   "plz" as your login_style, please provide your date of birth
+                   and plz in the following format: "DD/MM/YYYY/PLZ"
+    """
     def __init__(self, browser, login_style, username, password):
         self.browser = browser
         self.login_style = login_style
@@ -50,8 +66,38 @@ class Activator:
             pwd.send_keys(self.password)
             return None
         elif self.login_style == "pin":
-            pass
+            switch = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "toggleSecureLogin")))
+            switch.click()
+            switch = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "tabclassicLoginPin")))
+            switch.click()
+            name = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "cardnumberInputClassicPin")))
+            pwd  = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "pinInput")))
+            name.send_keys(self.username)
+            pwd.send_keys(self.password)
         elif self.login_style == "plz":
-            pass
+            switch = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "toggleSecureLogin")))
+            switch.click()
+            day, month, year, plz = self.credentials_plz()
+            name = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "cardnumberInputClassicDobZip")))
+            dob = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "dob")))
+            dob.click()
+            dayfield = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "dobDayName")))
+            monthfield = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "dobMonthName")))
+            yearfield = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "dobYearName")))
+            plzfield = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "zipName")))
+            name.send_keys(self.username)
+            dayfield.send_keys(day)
+            monthfield.send_keys(month)
+            yearfield.send_keys(year)
+            plzfield.send_keys(plz)
         else:
             print("when instantiating the object set login_style as password, pin or plz")
+            
+    def credentials_plz(self):
+        pwd = self.password
+        pwd = pwd.split("/")
+        day = pwd[0]
+        month = pwd[1]
+        year = pwd[2]
+        plz = pwd[3]
+        return day, month, year, plz
